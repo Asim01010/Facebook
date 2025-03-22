cab180ab90910daf5a76fdee4fc7142c0c2a0726
unknown <masimqw4hd@gmail.com>
2025-03-22 06:36:36 +0500

    new updates

--- src/component/auth/LoginForm.jsx
+++ src/component/auth/LoginForm.jsx
@@ -1,19 +1,108 @@
-import React from "react";
+import { Button } from "@mui/material";
+import { blue } from "@mui/material/colors";
+import React, { useEffect, useState } from "react";
+import { FaEye, FaRegEyeSlash } from "react-icons/fa";
+import { Link, Links } from "react-router-dom";
 
 const LoginForm = () => {
+  const [showeye, setShowEye] = useState(false);
+  const [showPass, setShowPass] = useState(false);
+  const [formField, setFormField] = useState({
+    email: "",
+    password: "",
+  });
+
+  // destructure it
+
+  const { email, password } = formField;
+
+  const btnHandle = (e) => {
+    setFormField({
+      ...formField,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (password.length > 0) {
+      setShowEye(true);
+    } else {
+      setShowEye(false);
+    }
+  }, [password]);
+
   return (
     <>
       <form className="p-2 shadow-xl rounded-md bg-white">
         <input
+          value={email}
+          name="email"
+          onChange={btnHandle}
           type="text"
-          className="w-full rounded-md border border-gray-400 outline-none focus:border-blue-500 p-3 "
+          className="w-full rounded-md border border-gray-400 outline-none focus:border-blue-500 p-3 my-3"
           placeholder="Email address or phone number"
         />
-        <input
-          type="password"
-          placeholder="Password"
-          className="w-full rounded-md border border-gray-400 outline-none focus:border-blue-500 p-3 "
-        />
+        <span className="relative">
+          <input
+            value={password}
+            name="password"
+            onChange={btnHandle}
+            type={showPass ? "text" : "password"}
+            placeholder="Password"
+            className={`w-full rounded-md border border-gray-400 outline-none focus:border-blue-500 p-3 mb-3`}
+          />
+
+          {showPass ? (
+            <FaEye
+              onClick={() => setShowPass(false)}
+              className={`absolute top-[50%] right-[5%] -translate-y-1/2 ${
+                !showeye && "hidden"
+              }`}
+              size={20}
+              cursor={"pointer"}
+            />
+          ) : (
+            <FaRegEyeSlash
+              onClick={() => setShowPass(true)}
+              className={`absolute top-[50%] right-[5%] -translate-y-1/2 ${
+                !showeye && "hidden"
+              }`}
+              size={20}
+              cursor={"pointer"}
+            />
+          )}
+        </span>
+        <Button
+          variant="contained"
+          className="w-full my-2"
+          style={{ padding: "12px", marginTop: "4px", fontWeight: "bold" }}
+        >
+          Login
+        </Button>
+        <Link
+          className="text-blue-500 hover:underline text-center block my-3
+         "
+        >
+          Forgotten account ?
+        </Link>
+        <hr className="border-0 bg-gray-300 h-[1px]" />
+        <Link to={"/"}>
+          <Button
+            variant="contained"
+            className="w-[50%] "
+            style={{
+              padding: "12px",
+              marginTop: "14px",
+              fontWeight: "bold",
+              background: "#36A420",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            Login
+          </Button>
+        </Link>
       </form>
     </>
   );
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -6,8 +6,8 @@ const Login = () => {
   return (
     <>
       <div className="min-h-screen justify-center items-center flex">
-        <div className="grid grid-cols-1 md:grid-cols-2 w-[90%] md:w-[70%] xl:w-[50%] lg:w-[60%] mx-auto">
-          <div className="flex  flex-col">
+        <div className="grid gap-5 grid-cols-1 md:grid-cols-2 w-[90%] md:w-[90%] xl:w-[60%] lg:w-[70%] mx-auto">
+          <div className="flex gap-3 flex-col">
             <img
               src="https://www.cdnlogo.com/logos/f/10/facebook.svg"
               alt="fb Logo"
